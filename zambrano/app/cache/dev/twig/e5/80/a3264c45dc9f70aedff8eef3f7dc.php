a6d09ee36724343901c0b827485e9daa8c4c2fb8
Antenas <Antenas@Antenas-PC>
2013-08-16 01:20:13 -0500

    ventas

---
+++ zambrano/app/cache/dev/twig/e5/80/a3264c45dc9f70aedff8eef3f7dc.php
@@ -0,0 +1,87 @@
+<?php
+
+/* ferreteriaZambranoBundle:Ventas:new.html.twig */
+class __TwigTemplate_e580a3264c45dc9f70aedff8eef3f7dc extends Twig_Template
+{
+    public function __construct(Twig_Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->parent = $this->env->loadTemplate("ferreteriaZambranoBundle::layout.html.twig");
+
+        $this->blocks = array(
+            'css' => array($this, 'block_css'),
+            'content' => array($this, 'block_content'),
+        );
+    }
+
+    protected function doGetParent(array $context)
+    {
+        return "ferreteriaZambranoBundle::layout.html.twig";
+    }
+
+    protected function doDisplay(array $context, array $blocks = array())
+    {
+        $this->parent->display($context, array_merge($this->blocks, $blocks));
+    }
+
+    // line 2
+    public function block_css($context, array $blocks = array())
+    {
+        // line 3
+        echo "    <link rel=\"stylesheet\" href=\"";
+        echo twig_escape_filter($this->env, $this->env->getExtension('assets')->getAssetUrl("bundles/ferreteriaZambrano/css/tablas_indice.css"), "html", null, true);
+        echo "\" type=\"text/css\" media=\"all\" />
+";
+    }
+
+    // line 6
+    public function block_content($context, array $blocks = array())
+    {
+        // line 7
+        echo "    <h1>Ventas creation</h1>
+
+    <form action=\"";
+        // line 9
+        echo twig_escape_filter($this->env, $this->env->getExtension('routing')->getPath("ventas_create"), "html", null, true);
+        echo "\" method=\"post\" ";
+        echo $this->env->getExtension('form')->renderer->searchAndRenderBlock($this->getContext($context, "form"), 'enctype');
+        echo ">
+        ";
+        // line 10
+        echo $this->env->getExtension('form')->renderer->searchAndRenderBlock($this->getContext($context, "form"), 'widget');
+        echo "
+
+        <p>
+            <button type=\"submit\">Create</button>
+        </p>
+    </form>
+
+        <ul class=\"record_actions\">
+    <li>
+        <a href=\"";
+        // line 19
+        echo twig_escape_filter($this->env, $this->env->getExtension('routing')->getPath("ventas"), "html", null, true);
+        echo "\">
+            Back to the list
+        </a>
+    </li>
+</ul>
+";
+    }
+
+    public function getTemplateName()
+    {
+        return "ferreteriaZambranoBundle:Ventas:new.html.twig";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  64 => 19,  52 => 10,  46 => 9,  42 => 7,  39 => 6,  32 => 3,  29 => 2,);
+    }
+}
